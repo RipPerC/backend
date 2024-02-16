061de87214b025d6ef41b6930f83c37b27f70c1b
C.RipPer <ripper.carina@gmail.com>
2024-02-16 20:24:09 +0000

    Eliminada clase no necesaria

--- src/main/java/org/home/backend/utilities/Constant.java
+++
@@ -1,7 +0,0 @@
-package org.home.backend.utilities;
-
-public class Constant {
-
-    public static String[] language = {"ES", "EN", "KO", "SE", "CAT", "DE", "FR", "IT"};
-    public static int numberRecords = 5;
-}
